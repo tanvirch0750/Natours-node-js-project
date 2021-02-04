349f6d5caabb0901e392407f8915294cc0d49086
tanvirch0750 <tanvir0750@yahoo.com>
2021-02-04 23:21:37 +0600

    handling patch request

--- app.js
+++ app.js
@@ -59,6 +59,24 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+// HANDLING PATCH REQUEST
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (+req.params.id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid Id',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here>',
+    },
+  });
+});
+
+// START APP
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port: ${port}`);
