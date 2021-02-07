716d462e708eb5e01f3e981b50d44e16b210042c
tanvirch0750 <tanvir0750@yahoo.com>
2021-02-07 20:39:15 +0600

    implemented environment variable and cofigure eslint and prettier

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // middleware
-app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
