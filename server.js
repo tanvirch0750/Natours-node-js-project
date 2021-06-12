7f63a949fa7e4048648923a4c6738abe59053433
tanvirch0750 <tanvir0750@yahoo.com>
2021-06-12 16:25:56 +0600

    creating a simple tour schema and atour model

--- server.js
+++ server.js
@@ -18,7 +18,23 @@ mongoose
 
 const app = require('./app');
 
-//console.log(process.env);
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 // START SERVER
 const port = process.env.PORT || 3000;
