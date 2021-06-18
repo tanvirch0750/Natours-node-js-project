bf732b6fdaf8a2e9462f709a8374a43a71900f18
tanvirch0750 <tanvir0750@yahoo.com>
2021-06-19 00:46:29 +0600

    Learned another method of creating documents

--- controller/tourController.js
+++ controller/tourController.js
@@ -3,17 +3,6 @@
 
 const Tour = require('../model/tourModel');
 
-// check body middleware
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Missing name or price',
-    });
-  }
-  next();
-};
-
 // ROUTE HANDLERS FOR TOURS
 // get
 exports.getAllTours = (req, res) => {
@@ -42,13 +31,22 @@ exports.getSingleTour = (req, res) => {
 };
 
 // post
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: 'success',
-    // data: {
-    //   tour: newTour,
-    // },
-  });
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data send',
+    });
+  }
 };
 
 // patch
