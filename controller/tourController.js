ea27477244c9c21a6884c773ac6956aacb9b100c
tanvirch0750 <tanvir0750@yahoo.com>
2021-06-20 17:24:14 +0600

    learned updating documents

--- controller/tourController.js
+++ controller/tourController.js
@@ -64,13 +64,25 @@ exports.createTour = async (req, res) => {
 };
 
 // patch
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here>',
-    },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
 
 // delete
