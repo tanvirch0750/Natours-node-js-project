1f88589c73fc36c7cb8566e022054c41f621ce6f
tanvirch0750 <tanvir0750@yahoo.com>
2021-07-11 21:05:53 +0600

    learn mongoose document middleware

--- model/tourModel.js
+++ model/tourModel.js
@@ -1,4 +1,6 @@
+/* eslint-disable prefer-arrow-callback */
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema({
   name: {
@@ -7,6 +9,7 @@ const tourSchema = new mongoose.Schema({
     unique: true,
     trim: true,
   },
+  slug: String,
   duration: {
     type: Number,
     required: [true, 'A tour must have a durations'],
@@ -60,6 +63,22 @@ tourSchema.set('toJSON', { virtuals: true });
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
+
+// DOCUMENT MIDDLEWARE
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.pre('save', function (next) {
+//   console.log('Will save document');
+//   next();
+// });
+
+// tourSchema.post('save', function (doc, next) {
+//   console.log(doc);
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
