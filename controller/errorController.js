6ae8e7d1116b055a1994eae6d6f11f52bbc5bf8c
tanvirch0750 <tanvir0750@yahoo.com>
2021-10-24 01:39:39 +0600

    Implemented error during development and production

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,9 +1,38 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.statusCode,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational error, send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.statusCode,
+      message: err.message,
+    });
+
+    // prograamming or other error, doesn't leak to client
+  } else {
+    console.error('ERROR', err);
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.statusCode,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
