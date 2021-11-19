b2830b386ad94f1a893b5ff9f7fb781345779bb4
tanvirch0750 <tanvir0750@yahoo.com>
2021-11-20 01:19:15 +0600

    logging in user

--- controller/userController.js
+++ controller/userController.js
@@ -1,10 +1,18 @@
+const User = require('../model/userModel');
+const catchAsync = require('../utils/catchAsync');
+
 // ROUTE HANDLERS FOR USERS
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not defined yet',
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users,
+    },
   });
-};
+});
 
 exports.createUser = (req, res) => {
   res.status(500).json({
--- model/userModel.js
+++ model/userModel.js
@@ -20,6 +20,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide a password'],
     minLength: 8,
+    select: false,
   },
   passwordConfirm: {
     type: String,
@@ -42,6 +43,13 @@ userSchema.pre('save', async function (next) {
 
   next();
 });
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 
