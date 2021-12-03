cf11d2acc68ea6061e34da58b365ba1caab64bbd
tanvirch0750 <tanvir0750@yahoo.com>
2021-12-03 19:47:46 +0600

    Sending jwt  via http only cookie

--- controller/authController.js
+++ controller/authController.js
@@ -15,6 +15,19 @@ const signToken = id =>
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
 
+  // Sending jwt via http-only-cookie
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('jwt', token, cookieOptions);
+
+  // Remove the password from output, so client won't see that
+  user.password = undefined;
+
   res.status(statusCode).json({
     status: 'success',
     token,
@@ -35,15 +48,6 @@ exports.signup = catchAsync(async (req, res, next) => {
   });
 
   createSendToken(newUser, 201, res);
-
-  // const token = signToken(newUser._id);
-  // res.status(201).json({
-  //   status: 'success',
-  //   token,
-  //   data: {
-  //     user: newUser,
-  //   },
-  // });
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -211,21 +215,3 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
   // 4) Log user in, send JWT
   createSendToken(user, 200, res);
 });
-
-// exports.updatePassword = catchAsync(async (req, res, next) => {
-//   // 1) Get user from the collection
-//   const user = await User.findById(req.user.id).select('+password');
-
-//   // 2) Check if POSTed current password is correct
-//   if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
-//     return next(new AppError('Your current password is wrong', 401));
-//   }
-
-//   // 3) If so, update the password
-//   user.password = req.body.password;
-//   user.passwordConfirm = req.body.passwordConfirm;
-//   await user.save();
-
-//   // 4) Log user in, send jwt
-//   createSendToken(user, 200, res);
-// });
